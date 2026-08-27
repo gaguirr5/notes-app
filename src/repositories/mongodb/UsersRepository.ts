56cff44355c5689626f0b68f30a6e7af657e6365
gaguirr5 <gaguirr5b@gmail.com>
2026-08-27 10:30:10 -0500

    refactor: apply document/domain type to user

--- src/repositories/mongodb/UsersRepository.ts
+++ src/repositories/mongodb/UsersRepository.ts
@@ -1,17 +1,20 @@
-import { User } from "@/types/User";
+import { User, UserDocument } from "@/types/User";
 import BaseRepository from "./BaseRepository";
+import { convertObjectIdToString } from "./mongoHelper";
 
-export default class UsersRepository extends BaseRepository<User> {
+export default class UsersRepository extends BaseRepository<UserDocument> {
   protected collectionName: string = "users";
 
   async findByEmail(email: string): Promise<User | null> {
     const collection = await this.getCollection();
-    return collection.findOne({ email });
+    const doc = await collection.findOne({ email });
+    if (!doc) return null;
+    return convertObjectIdToString(doc);
   }
 
   async create(user: Omit<User, "_id">): Promise<User> {
     const collection = await this.getCollection();
-    const result = await collection.insertOne(user);
-    return { ...user, _id: result.insertedId };
+    const result = await collection.insertOne(user as any); //need to look at this with OptionalId
+    return { ...user, _id: result.insertedId.toString() };
   }
 }
